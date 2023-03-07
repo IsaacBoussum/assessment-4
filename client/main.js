e462762a1575af00093186812bbbceae866bb3b2
Isaac Boussum <isaacboussum10@gmail.com>
2023-03-06 20:55:48 -0700

    first attempt

---
+++ client/main.js
@@ -0,0 +1,66 @@
+
+
+
+const complimentBtn = document.getElementById("complimentButton")
+const fortuneBtn = document.getElementById("fortuneButton");
+const goalForm = document.getElementById("goalForm");
+const goalInput = document.getElementById("goalInput");
+const deadlineInput = document.getElementById("deadlineInput");
+const goalList = document.getElementById("goalList");
+const quoteButton = document.getElementById("quote-Button");
+const quoteDisplay = document.getElementById("quoteDisplay");
+
+const getCompliment = () => {
+    axios.get("http://localhost:4000/api/compliment/")
+        .then(res => {
+            const data = res.data;
+            alert(data);
+    });
+};
+
+const getFortune = () => {
+    axios.get("http://localhost:4000/api/fortune/")
+        .then(res => {
+            const data = res.data;
+            alert(data);
+    });
+};
+
+const getQuote = () => {
+    axios.get("http://localhost:4000/api/quote/")
+        .then(res => {
+            const data = res.data;
+            quoteDisplay.textContent = data;
+    });
+};
+
+const submitGoal = (event) => {
+    event.preventDefault();
+
+    const goal = goalInput.value;
+    const deadline = deadlineInput.value;
+
+    axios.post("http://localhost:4000/api/goals", { goal, deadline })
+        .then(res => {
+            
+            const newGoal = res.data;
+
+            const listItem = document.createElement("li");
+            listItem.innerText = `${newGoal.goal}  ${newGoal.deadline}`;
+            goalList.appendChild(listItem);
+
+            goalInput.value = "";
+            deadlineInput.value = "";
+        });
+};
+
+
+
+
+
+
+
+goalForm.addEventListener('submit', submitGoal)
+complimentBtn.addEventListener('click', getCompliment);
+fortuneBtn.addEventListener("click", getFortune);
+quoteButton.addEventListener('click', getQuote);
---
+++ server/controller.js
@@ -0,0 +1,84 @@
+const goals = [];
+
+module.exports = {
+
+    getCompliment: (req, res) => {
+        const compliments = ["Gee, you're a smart cookie!", "Cool shirt!", "Your Javascript skills are stellar."];
+      
+        // choose random compliment
+        let randomIndex = Math.floor(Math.random() * compliments.length);
+        let randomCompliment = compliments[randomIndex];
+      
+        res.status(200).send(randomCompliment);
+    },
+
+    getFortune: (req, res) => {
+        const fortunes = [
+            "A dubious friend may be an enemy in camouflage.", 
+            "A beautiful, smart, and loving person will be coming into your life.", 
+            "A faithful friend is a strong defense.", 
+            "A feather in the hand is better than a bird in the air.", 
+            "A fresh start will put you on your way."
+        ];
+        
+        let randomIndex = Math.floor(Math.random() * fortunes.length);
+        let randomFortune = fortunes[randomIndex];
+
+        res.status(200).send(randomFortune);
+    },
+
+    getQuote: (req, res) => {
+        const quotes = [
+            "You miss 100% of the shots you don't take. - Wayne Gretzky",
+            "I have not failed. I've just found 10,000 ways that won't work. - Thomas Edison",
+            "Believe you can and you're halfway there. - Theodore Roosevelt",
+            "It does not matter how slowly you go as long as you do not stop. - Confucius",
+            "The only way to do great work is to love what you do. - Steve Jobs"
+        ];
+        
+        let randomIndex = Math.floor(Math.random() * quotes.length);
+        let randomQuote = quotes[randomIndex];
+
+        res.status(200).send(randomQuote);
+    },
+    
+    addGoal: (req, res) => {
+        const { goal, deadline } = req.body;
+        if (!goal || !deadline) {
+          return res.status(400).send('Both goal and deadline are required!');
+        }
+        const newGoal = {
+          id: goals.length + 1,
+          goal,
+          deadline,
+          completed: false
+        };
+        goals.push(newGoal);
+        res.status(201).send(newGoal);
+      },
+    
+      getGoals: (req, res) => {
+        res.status(200).send(goals);
+      },
+    
+      completeGoal: (req, res) => {
+        const { id } = req.params;
+        const goal = goals.find((e) => e.id === parseInt(id));
+        if (!goal) {
+          return res.status(404).send(`Goal with id ${id} not found!`);
+        }
+        goal.completed = true;
+        res.status(200).send(goal);
+      },
+    
+      deleteGoal: (req, res) => {
+        const { id } = req.params;
+        const index = goals.findIndex((e) => e.id === parseInt(id));
+        if (index === -1) {
+          return res.status(404).send(`Goal with id ${id} not found!`);
+        }
+        goals.splice(index, 1);
+        res.status(200).send(`Goal with id ${id} has been deleted!`);
+      },
+    
+}
